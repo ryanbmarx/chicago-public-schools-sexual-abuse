e9724db32a7dc68c6cd6c56f634756814c73080e
ryanbmarx <ryanbmarx@gmail.com>
2018-04-19 17:11:27 -0500

    Update initial tarbell_config.py with additional excludes

--- tarbell_config.py
+++ tarbell_config.py
@@ -8,7 +8,7 @@ Tarbell project configuration
 SPREADSHEET_KEY = "1SQ_N8fyaimSvjMs62HyATD1CsnssDD7fVWKp14Ta7eQ"
 
 # Exclude these files from publication
-EXCLUDES = ["*.md", "requirements.txt"]
+EXCLUDES = ['*.md', 'requirements.txt', 'node_modules', 'sass', 'js/src', '*.ai', 'package.json', 'Gruntfile.js']
 
 # Spreadsheet cache lifetime in seconds. (Default: 4)
 # SPREADSHEET_CACHE_TTL = 4
