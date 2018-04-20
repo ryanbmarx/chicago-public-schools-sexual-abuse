2939f4fd1eaea0a4ffe59eb7c246e061c9b97453
ryanbmarx <ryanbmarx@gmail.com>
2018-04-20 13:21:12 -0500

    watch blueprint for sass changes, too

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,7 +93,7 @@ module.exports = function(grunt) {
 
   config.watch = {
     sass: {
-      files: ['sass/**/*.scss'],
+      files: ['sass/**/*.scss', '_blueprint/**/*.scss'],
       tasks: ['sass', 'postcss']
     },
     js: {
