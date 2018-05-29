cc840501b612a5d511c50e04e442b5eec855c59d
ryanbmarx <ryanbmarx@gmail.com>
2018-05-29 16:18:04 -0500

    correct very stupid error

--- js/src/click-track.js
+++ js/src/click-track.js
@@ -1,25 +1,29 @@
 // A snippet to provide an omniture custom link track
 
-module.exports = function clickTrack(uniquePhrase, isALink=false, debug=false){
-	if (debug) console.log(`Tracking ${uniquePhrase}`);
+function clickTrack(uniquePhrase, isALink=false, debug=false){
 	try{
-		linkType = isALink ? true : this;
-
+		console.log("inside try");
+		 // madeUpFunc(false);
+		// If you're not actually tracking a link click, use true instead of `this` to supress a delay
+		const linkType = isALink ? true : this;
 		s.linkTrackVars = "server,prop3,prop20,prop28,prop33,prop34,prop57,eVar3,eVar20,eVar21,eVar34,eVar35,eVar36,eVar37,eVar38,eVar39,eVar51";
 		s.linkTrackEvents = "";
 		s.prop57 = uniquePhrase;
-		s.tl(
-		   // Since we're not actually tracking a link click, use true instead of `this`.  This also supresses a delay
-		  linkType,
 		   // linkType, 'o' for custom link
-		   'o',
-		   // linkName
-			uniquePhrase,
-		   // variableOverrides
-		   null
-		);
+		s.tl(linkType,'o',uniquePhrase, null);
 	}
-	catch (ReferenceError){
+	// catch (ReferenceError){
+	// 	console.warn('You must be running this locally. *OR* Omniture is not loaded. Skipping analytics.');
+	// }
+
+	catch (error){
 		console.warn('You must be running this locally. *OR* Omniture is not loaded. Skipping analytics.');
+		console.error(error);
+	}
+
+	finally{
+		if (debug) console.log(`Tracking ${uniquePhrase}`);
 	}
 }
+
+module.exports = clickTrack;
